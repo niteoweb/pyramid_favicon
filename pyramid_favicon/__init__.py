1a56f3ce6db405a85515ad995d362e9e1e2f74ec
Paolo Barazon <paolo.barazon@stableapps.com>
2016-01-01 12:54:51 +0800

    remove unused code

--- pyramid_favicon/__init__.py
+++ pyramid_favicon/__init__.py
@@ -3,7 +3,4 @@
 
 def importme(config):
     config.add_route('favicon', '/favicon.ico')
-
-    # settings  = config.registry.settings
-    # favicon_folder = settings.get('favicon_folder', None)
     
