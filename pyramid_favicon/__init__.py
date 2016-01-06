75d69a58482c23bb2aed075e91a64e33a2538381
Paolo Barazon <paolo.barazon@stableapps.com>
2016-01-06 21:17:23 +0800

    add directives for custom config

--- pyramid_favicon/__init__.py
+++ pyramid_favicon/__init__.py
@@ -4,3 +4,6 @@ from favicon import favicon
 def includeme(config):
     config.add_route('favicon', '/favicon.ico')
     config.add_view('pyramid_favicon.favicon', route_name='favicon')
+
+    settings = config.registry.settings
+    favicon_path = settings.get('favicon_path', None)
