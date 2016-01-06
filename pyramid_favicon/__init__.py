3ff8573f3347de5333f2bbcac792936ed020b0f9
Paolo Barazon <paolo.barazon@stableapps.com>
2016-01-06 20:24:04 +0800

    fix error in resolving view name

--- pyramid_favicon/__init__.py
+++ pyramid_favicon/__init__.py
@@ -1,6 +1,6 @@
 # __init__.py
 from favicon import favicon
 
-def includeme(config):	
+def includeme(config):
     config.add_route('favicon', '/favicon.ico')
-    config.add_route('home', '/')
+    config.add_view('pyramid_favicon.favicon', route_name='favicon')
