50514a37f14f3f0ddc36a369623f4ec0b338a785
Paolo Barazon <paolo.barazon@stableapps.com>
2016-01-15 00:12:23 +0800

    bump up version number

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name = 'pyramid_favicon',
 
     # version
-    version = '0.0.5',
+    version = '0.0.6',
 
     # description
     description = 'Pyramid add-on for defining relocatable favicon.ico',
